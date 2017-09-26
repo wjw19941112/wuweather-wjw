45cc85303b0729a43f2fdebc1e9b89310a0eda08
wjw19941112 <wujiwei@pku.edu.cn>
2017-09-27 00:49:21 +0800

    wuweather

---
+++ app/src/main/java/com/example/wu/wuweather/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.wu.wuweather;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by wu on 2017/9/26.
+ */
+
+public class MainActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
