2b34e7b6b5958616fc8a85bd9efb6c790a733d40
wjw19941112 <wujiwei@pku.edu.cn>
2017-12-01 10:01:54 +0800

    仅第一次启动显示导航页

--- app/src/main/java/com/example/wu/wuweather/Guide.java
+++ app/src/main/java/com/example/wu/wuweather/Guide.java
@@ -2,6 +2,7 @@ package com.example.wu.wuweather;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
@@ -28,6 +29,19 @@ public class Guide extends Activity implements ViewPager.OnPageChangeListener{
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.guide);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("viewpagedate",Activity.MODE_PRIVATE);
+        if(sharedPreferences.getInt("time",0)==10)//
+        {
+            Intent intent = new Intent(Guide.this,MainActivity.class);
+            startActivity(intent);
+            finish();
+        }else {
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putInt("time", 10);//0
+            editor.commit();
+        }
+
         initViews();
         initDots();
         btn = (Button)views.get(2).findViewById(R.id.btn);
